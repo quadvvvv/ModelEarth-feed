fb98969c5afe9c8184c4fcbe287d75a750aad549
modelearth <model@dreamstudio.com>
2024-05-30 10:06:50 -0400

    blank base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/feed/dist/', // Set the base path here
+  base: '', // Set the base path here  /feed/dist/
 })
