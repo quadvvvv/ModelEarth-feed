d7bdf0ba3cbbf9d5598022e6eb310dfd46974837
modelearth <model@dreamstudio.com>
2024-06-17 15:49:09 -0400

    Next version of vite.config.js

---
+++ vite.config-upcoming.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import copyAssets from './vite-plugin-copy';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    //copyAssets()
+  ],
+  base: '',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+});
