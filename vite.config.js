7141ed8a6d138174d546de600aaaa38b672501e0
modelearth <model@dreamstudio.com>
2024-09-23 23:47:48 -0400

    Use feedplayer consistents names for widget

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,41 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import copyAssets from './vite-plugin-copy';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    copyAssets()
+    copyAssets(),
+    renameHtml() // Add the plugin here
   ],
-  base: '', // base path was originally not set. Another option is /feed/dist/
+  base: '', // base path
   build: {
     rollupOptions: {
+      input: "./feedplayer.html",
       output: {
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
+        //assetFileNames: 'assets/[name]-[hash].[ext]',
+        //chunkFileNames: 'assets/[name]-[hash].js',
+        //entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
       },
     },
   },
 });
+
+function renameHtml() {
+  return {
+    name: 'rename-html',
+    writeBundle() {
+      const oldPath = path.resolve(__dirname, 'dist/feedplayer.html');
+      const newPath = path.resolve(__dirname, 'dist/index.html');
+
+      if (fs.existsSync(oldPath)) {
+        fs.renameSync(oldPath, newPath);
+      }
+    },
+  };
+}
