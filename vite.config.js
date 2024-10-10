0b433b1f40e3b7308459c7b58adb3ec5290385e3
yni <yn2443@columbia.edu>
2024-10-09 20:21:00 -0400

    bug: Fixed videoplayer full screen mode; Updated the "yarn dev" logic with the same feedplayer.html as "yarn dev".

--- vite.config.js
+++ vite.config.js
@@ -3,37 +3,52 @@ import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    renameHtml() // Add the plugin here
-  ],
-  base: '', // Otherwise '/assets' is the default
-  build: {
-    rollupOptions: {
-      input: "./feedplayer.html",
-      output: {
-        //assetFileNames: 'assets/[name]-[hash].[ext]',
-        //chunkFileNames: 'assets/[name]-[hash].js',
-        //entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js',
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+  
+  return {
+    plugins: [
+      react(),
+      {
+        name: 'handle-html',
+        configureServer(server) {
+          server.middlewares.use((req, res, next) => {
+            if (req.url === '/' || req.url === '/index.html') {
+              req.url = '/feedplayer.html';
+            }
+            next();
+          });
+        },
+        transformIndexHtml(html, { filename }) {
+          if (isDev && path.basename(filename) === 'index.html') {
+            return fs.readFileSync('./feedplayer.html', 'utf-8');
+          }
+          return html;
+        },
+      },
+      {
+        name: 'rename-html',
+        apply: 'build',
+        writeBundle() {
+          const oldPath = path.resolve(__dirname, 'dist/feedplayer.html');
+          const newPath = path.resolve(__dirname, 'dist/index.html');
+
+          if (fs.existsSync(oldPath)) {
+            fs.renameSync(oldPath, newPath);
+          }
+        },
+      },
+    ],
+    base: '',
+    build: {
+      rollupOptions: {
+        input: './feedplayer.html',
+        output: {
+          assetFileNames: 'assets/[name].[ext]',
+          chunkFileNames: 'assets/[name].js',
+          entryFileNames: 'assets/[name].js',
+        },
       },
     },
-  },
-});
-
-function renameHtml() {
-  return {
-    name: 'rename-html',
-    writeBundle() {
-      const oldPath = path.resolve(__dirname, 'dist/feedplayer.html');
-      const newPath = path.resolve(__dirname, 'dist/index.html');
-
-      if (fs.existsSync(oldPath)) {
-        fs.renameSync(oldPath, newPath);
-      }
-    },
   };
-}
+});
